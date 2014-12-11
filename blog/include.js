a7f92b3c83b2f231c195e66930b68949151d7376
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-12-11 00:15:34 -0500

    Documentation

--- blog/include.js
+++ blog/include.js
@@ -20,12 +20,38 @@
         blogCssUrl = baseUrl + "/blog/blog.css";
     }
 
-    window.setTimeout(function () {
+    /**
+     * Helper to create an HTML element with attributes
+     *
+     * @param {String] name Element name
+     * @param {Object} attributes Dictionary of name to value
+     * @returns {*}
+     * @private
+     */
+    function _el(name, attributes) {
+        var
+            result = document.createElement(name),
+            att;
+        for (att in attributes) {
+            if (attributes.hasOwnProperty(att)) {
+                result.setAttribute(att, attributes[att]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Include scripts & resources needed to render the blog the right way.
+     * NOTE: if a header script element is found pointing to the GPF library,
+     * it means _include has already been executed.
+     *
+     * @private
+     */
+    function _include() {
         var
             head = document.getElementsByTagName("head")[0]
                    || document.documentElement,
             scripts = head.getElementsByTagName("script"),
-            tag,
             idx;
         for (idx = 0; idx < scripts.length; ++idx) {
             if (scripts[idx].src === gpfUrl) {
@@ -33,37 +59,29 @@
             }
         }
         // Not inserted yet
-        tag = function (name, attributes) {
-            var
-                result = document.createElement(name),
-                att;
-            for (att in attributes) {
-                if (attributes.hasOwnProperty(att)) {
-                    result.setAttribute(att, attributes[att]);
-                }
-            }
-            return result;
-        };
-        head.insertBefore(tag("script", {
+        head.insertBefore(_el("script", {
             language: "javascript",
             src: gpfUrl
         }), head.firstChild);
-        head.insertBefore(tag("script", {
+        head.insertBefore(_el("script", {
             language: "javascript",
             src: blogJsUrl
         }), head.firstChild);
         if (fileAccess) {
-            head.appendChild(tag("link", {
+            head.appendChild(_el("link", {
                 rel: "stylesheet",
                 type: "text/css",
                 href: "../file.css"
             }));
         }
-        head.appendChild(tag("link", {
+        head.appendChild(_el("link", {
             rel: "stylesheet",
             type: "text/css",
             href: blogCssUrl
         })); // Must be the last one
-    }, 0);
+    }
+
+    // Defer execution of _include to finish page loading first
+    window.setTimeout(_include, 0);
 
 }());
