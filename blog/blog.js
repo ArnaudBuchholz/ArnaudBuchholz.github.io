7439fecafb3e9056ea207248bbb0d08ce797a88e
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-12-11 00:30:44 -0500

    Documentation

--- blog/blog.js
+++ blog/blog.js
@@ -1,64 +1,74 @@
 (function () {
     "use strict";
 
+    /**
+     * Event handler for the tokenizer
+     *
+     * @param {gpf.events.Event} event
+     */
     function onTokenFound (event) {
-        // Trim any space token before the first non space one
+        /*jshint -W040*/ // this is the transmitted by gpf.js.tokenize
         var
-            that = this,
-            type,
-            token;
-        if (gpf.events && event instanceof gpf.events.Event) {
-            type = event.type();
-            token = event.get("token");
-        } else {
-            // Backward compatibility
-            type = arguments[0];
-            token = arguments[1];
-        }
+            codeElement = this,
+            type = event.type(),
+            token = event.get("token"),
+            tag;
         if ("space" === type) {
-            if (!that.hasChildNodes()) {
+            // Trim any space token before the first non space one
+            if (!codeElement.hasChildNodes()) {
                 return;
             }
             // Replace tabs with 4 spaces
-            token.replace(/\t/, "    ");
+            token = gpf.replaceEx(token, {
+                "\t": "    "
+            });
         }
-        var tag = document.createElement("span");
+        // Concatenate to the code ele,ent
+        tag = document.createElement("span");
         tag.className = type;
         tag.appendChild(document.createTextNode(token));
-        that.appendChild(tag);
+        codeElement.appendChild(tag);
     }
 
+    /**
+     * Reformat the code element
+     *
+     * @param {HTMLDomElement} codeElement
+     */
     function reformatCode (codeElement) {
         var
             codeClass = codeElement.className,
-            pre/*, src, toolbar*/;
+            pre;
+        /**
+         * Transform the <CODE class="X"></CODE> structure into the following:
+         * <PRE class="code X"><CODE class="X"></CODE></PRE>
+         */
         pre = document.createElement("pre");
         pre.className = "code " + codeClass;
         pre = codeElement.parentNode.insertBefore(pre, codeElement);
-        pre.appendChild(codeElement);
+        codeElement = pre.appendChild(codeElement);
+        // Use tokenizer to format the content
         if ("javascript" === codeClass) {
             // https://github.com/ArnaudBuchholz/gpf-js/issues/5
             var content = codeElement.innerHTML
                             .replace(/(&lt;)/g, "<")
                             .replace(/(&gt;)/g, ">")
-                            .replace(/(&amp;)/g, "&")
-                ;
-            codeElement.innerHTML = ""; // Easy way to clear this
-            if (gpf.tokenize) {
-                // Backward compatibility
-                gpf.tokenize.apply(codeElement, [content, onTokenFound]);
-            } else {
-                gpf.js.tokenize.apply(codeElement, [content, onTokenFound]);
-            }
-/*
-    Could be great to have testable samples
-            // Insert a toolbar
-            toolbar = document.createElement("div");
-            toolbar.className = "toolbar";
-            pre.insertBefore(toolbar, codeElement);
-*/
+                            .replace(/(&amp;)/g, "&");
+            codeElement.innerHTML = ""; // Easy way to clear current content
+            gpf.js.tokenize.apply(codeElement, [content, onTokenFound]);
         }
     }
+
+    // Create a waiting dialog that will appear / disappear on need
+    // It remains on top of the screen (i.e. follow scroll)
+    var _waitingDialog = document.createElement("div");
+    _waitingDialog.className = "waiting";
+    _waitingDialog.innerHTML = "<span class=\"gear spin\"></span>"
+                               + "<span class=\"label\">Waiting for GPF</span>";
+    _waitingDialog = document.body.appendChild(_waitingDialog);
+
+    // As the blog may load posts asynchronously, monitor the DOM layout
+    // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 
     function waitForLoad () {
         if ("undefined" === typeof gpf
@@ -69,15 +79,7 @@
         }
         var
             codes = document.getElementsByTagName("code"),
-            idx,
-            progressDiv;
-        if (codes.length) {
-            progressDiv = document.createElement("div");
-            progressDiv.className = "code_formatting";
-            progressDiv.innerHTML = "<span class=\"gear spin\"></span>"
-                + "<span>Formatting code...</span>";
-            document.body.appendChild(progressDiv);
-        }
+            idx;
         for (idx = 0; idx < codes.length; ++idx) {
             reformatCode(codes[idx]);
         }
