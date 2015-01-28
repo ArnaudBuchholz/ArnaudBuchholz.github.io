4714c0287afa63c8b9c725333fb36a512c8c0cfe
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-28 00:14:27 -0500

    Modified to match the article

--- blog/post/Code analysis/sample1.js
+++ blog/post/Code analysis/sample1.js
@@ -2,17 +2,26 @@
     "use strict";
 
     /*global $, freezeInterface, updateInterfaceUsingData, unfreezeInterface*/
+    /*global showError*/
 
     // Handles the click on a specific button
     function onClick () {
-
         freezeInterface("Please wait, updating information...");
         $.ajax("getInformation.aspx", {
             success: function (data) {
-                updateInterfaceUsingData(data);
+                var resultCode = data.resultCode;
+                if (resultCode === 0) {
+                    updateInterfaceUsingData(data);
+                } else {
+                    // The 'error' keyword is here but inside a string
+                    showError("An error occurred");
+                }
             }
         });
         unfreezeInterface();
     }
+
+    document.getElementById("myButton")
+        .addEventListener("click", onClick);
 
 })();
