1bf615284112c8695ffec3d0195dbb7a3893b637
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-01-28 00:14:38 -0500

    Created to remove errors

---
+++ blog/post/Code analysis/sample2.js
@@ -0,0 +1,31 @@
+(function () {
+    "use strict";
+
+    /*global $, freezeInterface, updateInterfaceUsingData, unfreezeInterface*/
+    /*global showError*/
+
+    // Handles the click on a specific button
+    function onClick () {
+        freezeInterface("Please wait, updating information...");
+        $.ajax("getInformation.aspx", {
+            success: function (data) {
+                var resultCode = data.resultCode;
+                if (resultCode === 0) {
+                    updateInterfaceUsingData(data);
+                } else {
+                    // The 'error' keyword is here but inside a string
+                    showError("An error occurred");
+                }
+                unfreezeInterface();
+            },
+            error: function () {
+                showError("An error occurred");
+                unfreezeInterface();
+            }
+        });
+    }
+
+    document.getElementById("myButton")
+        .addEventListener("click", onClick);
+
+})();
