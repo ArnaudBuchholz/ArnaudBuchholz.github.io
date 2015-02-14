49971bdcd3b794154e122cec26495f1e6d103e1c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2015-02-14 09:06:24 -0500

    Sample 5

--- blog/post/Asynchronous synchronisation/sample5.js
+++ blog/post/Asynchronous synchronisation/sample5.js
@@ -2,56 +2,56 @@ var fs = require("fs"),
     path = require("path"),
     start = new Date(),
     key,
-    count;
+    count,
+    pendingCount = 0;
 
-function exploreAsync(currentPath, callback) {
+function exploreAsync(currentPath, callback, result) {
     "use strict";
-    var
-        result = {},
-        pendingCount = 0;
-    function _explore(currentPath) {
-        ++pendingCount;
-        fs.stat(currentPath, function (err, stat) {
-            if (err) {
-                console.error(err);
-            } else if (stat && stat.isDirectory()) {
-                // Folder
-                ++pendingCount;
-                fs.readdir(currentPath, function (err, list) {
-                    var
-                        len,
-                        idx;
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        len = list.length;
-                        for (idx = 0; idx < len; ++idx) {
-                            _explore(path.join(currentPath, list[idx]));
-                        }
-                    }
-                    if (0 === --pendingCount) {
-                        callback(result);
-                    }
-                });
-            } else {
-                ++pendingCount;
-                fs.readFile(currentPath, function (err, data) {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        result[currentPath] = data;
-                    }
-                    if (0 === --pendingCount) {
-                        callback(result);
-                    }
-                });
-            }
-            if (0 === --pendingCount) {
-                callback(result);
-            }
-        });
+    if (undefined === result) {
+        // Allocate result dictionary that will be passed to the sub calls
+        result = {};
     }
-    _explore(currentPath);
+    ++pendingCount;
+    fs.stat(currentPath, function (err, stat) {
+        if (err) {
+            console.error(err);
+        } else if (stat && stat.isDirectory()) {
+            // Folder
+            ++pendingCount;
+            fs.readdir(currentPath, function (err, list) {
+                var
+                    len,
+                    idx;
+                if (err) {
+                    console.error(err);
+                } else {
+                    len = list.length;
+                    for (idx = 0; idx < len; ++idx) {
+                        exploreAsync(path.join(currentPath, list[idx]),
+                            callback, result);
+                    }
+                }
+                if (0 === --pendingCount) {
+                    callback(result);
+                }
+            });
+        } else {
+            ++pendingCount;
+            fs.readFile(currentPath, function (err, data) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    result[currentPath] = data;
+                }
+                if (0 === --pendingCount) {
+                    callback(result);
+                }
+            });
+        }
+        if (0 === --pendingCount) {
+            callback(result);
+        }
+    });
 }
 
 function result (files) {
@@ -70,4 +70,3 @@ function result (files) {
 exploreAsync(process.argv[2], result);
 exploreAsync(process.argv[2], result);
 exploreAsync(process.argv[2], result);
-
