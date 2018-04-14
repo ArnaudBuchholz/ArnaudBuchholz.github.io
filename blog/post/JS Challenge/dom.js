e16c3092546a9d316c0392edd5e601b0543698ff
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-04-14 10:16:09 -0400

    DOM helpers

---
+++ blog/post/JS Challenge/dom.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var dom = {
+
+    head: function () {
+        return document.querySelector("head");
+    }
+
+};
+
+[
+    "a",
+    "button",
+    "div",
+    "h1",
+    "input",
+    "label",
+    "li",
+    "link",
+    "nav",
+    "optgroup",
+    "option",
+    "p",
+    "script",
+    "select",
+    "span",
+    "strong",
+    "ul"
+
+].forEach(function (tagName) {
+    dom[tagName] = gpf.web.createTagFunction(tagName);
+});
+
+module.exports = dom;
