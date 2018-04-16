87515b2de5e7ff4395f350ad012ae6300d536a2f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-16 13:36:23 -0400

    Adds showdown and wait for the libs to be loaded

--- blog/post/JS Challenge/libs.js
+++ blog/post/JS Challenge/libs.js
@@ -5,23 +5,30 @@ var dom = require("dom.js"),
     link = dom.link,
     head = dom.head();
 
-script({
-    src: "https://code.jquery.com/jquery-3.3.1.min.js",
-    integrity: "sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=",
-    crossorigin: "anonymous"
+function waitFor(scriptDef) {
+    return new Promise(function (resolve) {
+        script(scriptDef).appendTo(head).addEventListener("load", function () {
+            resolve();
+        });
+    });
+}
 
-}).appendTo(head).addEventListener("load", function () {
-
-    // jQuery is required for bootstrap
-
-    script({
-        src: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js",
-        integrity: "sha384-lZmvU/TzxoIQIOD9yQDEpvxp6wEU32Fy0ckUgOH4EIlMOCdR823rg4+3gWRwnX1M",
+var jQueryAndBootstrap = waitFor({
+        src: "https://code.jquery.com/jquery-3.3.1.min.js",
+        integrity: "sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=",
         crossorigin: "anonymous"
 
-    }).appendTo(head);
+    }).then(function () {
 
-});
+        // jQuery is required for bootstrap
+        return waitFor({
+            src: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js",
+            integrity: "sha384-lZmvU/TzxoIQIOD9yQDEpvxp6wEU32Fy0ckUgOH4EIlMOCdR823rg4+3gWRwnX1M",
+            crossorigin: "anonymous"
+
+        });
+
+    });
 
 link({
     href: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css",
@@ -31,13 +38,22 @@ link({
 
 }).appendTo(head);
 
-script({
+var prism = waitFor({
     src: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/prism.js"
 
-}).appendTo(head);
+});
 
 link({
     href: "https://cdnjs.cloudflare.com/ajax/libs/prism/1.13.0/themes/prism.css",
     rel: "stylesheet"
 
 }).appendTo(head);
+
+var showdown = waitFor({
+    src: "https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.6/showdown.js",
+    integrity: "sha256-WZhItMKyEJQarLzuYWKmNplzBgqEKvlAedjRsChG3JA=",
+    crossorigin: "anonymous"
+
+});
+
+module.exports = Promise.all([jQueryAndBootstrap, prism, showdown]);
