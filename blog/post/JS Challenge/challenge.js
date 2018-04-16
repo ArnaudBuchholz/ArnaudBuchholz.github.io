3446b3248b799935f11e0f79930139e2bf425026
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-04-16 14:56:43 -0400

    Implements assertions

--- blog/post/JS Challenge/challenge.js
+++ blog/post/JS Challenge/challenge.js
@@ -13,7 +13,7 @@ gpf.require.define({
         })[0] || {title: "Unknown"}).title,
         codes = $("code"),
         description = $(codes[0]).text(),
-        javascript = $(codes[1]).text(),
+        source = $(codes[1]).text().trim(),
         converter = new showdown.Converter()
     ;
 
@@ -21,6 +21,45 @@ gpf.require.define({
     document.title = title;
     $("#title").text(title);
     $("#content").html(converter.makeHtml(description));
-    $("#code").html(Prism.highlight(javascript, Prism.languages.javascript, "html"));
+
+    window.assert = function (condition, message) {
+        if (!condition) {
+            if (message) {
+                throw new Error("Assertion failed: " + message);
+            }
+            throw new Error("An assertion failed");
+        }
+    }
+
+    window.onerror = function (e) {
+        $("#error").text(e.toString());
+        $(".alert-danger").show();
+        $(".alert-success").hide();
+    }
+
+    $("#proposal").keypress(function (e) {
+        if(13 === e.which) {
+            $("#propose").click();
+        }
+    });
+
+    $("#propose").on("click", function () {
+        $(".alert").hide();
+        var proposal = $("#proposal").val(),
+            modifiedSource = source.replace("your_solution_here", $("#proposal").val());
+        $("#code").html(Prism.highlight(modifiedSource, Prism.languages.javascript, "html"));
+        $("#email").attr("href", "mailto:arnaud.buchholz@free.fr?subject="
+            + encodeURIComponent("[JS-Challenge:" + id + "] I solved it with: " + proposal));
+        jQuery.globalEval([
+            // Disable all global symbols but assert
+            "(function (__proto__, " + Object.keys(window).filter(function (name) { return name !== "assert"; }).join(", ") + "){",
+            modifiedSource.replace(/[^."]assert\((.*)\);/g, function (match, condition) {
+                return match.substr(0, match.length - 2) + ", \"" + condition.split("\\").join("\\\\").split("\"").join("\\\"") + "\");";
+            }),
+            "}());"
+        ].join("\n"));
+    });
+
+    $("#propose").click();
 
 });
