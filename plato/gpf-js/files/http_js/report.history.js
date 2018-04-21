ab2e7bfad4f17663831a8c9fc4cef3b8aab332b7
Arnaud Buchholz <arnaud.buchholz@free.fr>
2018-04-21 12:11:23 -0400

    Additional cleaning

--- plato/gpf-js/files/http_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Thu, 19 Feb 2015 14:02:10 GMT","sloc":255,"lloc":112,"functions":11,"deliveredBugs":1.2321978325543752,"maintainability":62.23352222583692,"lintErrors":[],"difficulty":40.4},{"date":"Thu, 19 Feb 2015 18:07:03 GMT","sloc":255,"lloc":112,"functions":11,"deliveredBugs":1.2321978325543752,"maintainability":62.23352222583692,"lintErrors":[],"difficulty":40.4}]
