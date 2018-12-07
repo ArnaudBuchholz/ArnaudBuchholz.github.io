e50cc5170ccfb0dfc5645e913c4e98c2447b4785
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-12-07 13:10:32 -0500

    Export the generated HTML (goal is to get ride of md->html on the blog)

---
+++ blog/export.js
@@ -0,0 +1,15 @@
+"use strict";
+
+window.addEventListener("load", function () {
+    var button = document.createElement("button");
+    button.className = "export";
+    button.innerHTML = "Export";
+    button = document.body.insertBefore(button, document.body.firstChild);
+    var input = document.body.appendChild(document.createElement("textarea"));
+    // input.className = "hidden";
+    button.addEventListener("click", function () {
+        input.value = document.querySelector(".post-body").innerHTML;
+        input.select();
+        document.execCommand("copy");
+    });
+});
