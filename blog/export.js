3a31070f616ea4eabc2500c6cfe933a411077e8c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-12-07 13:45:36 -0500

    Remove todo helpers

--- blog/export.js
+++ blog/export.js
@@ -7,7 +7,15 @@ window.addEventListener("load", function () {
     button = document.body.insertBefore(button, document.body.firstChild);
     var input = document.body.appendChild(document.createElement("textarea"));
     // input.className = "hidden";
+    function remove (selector) {
+        var node = document.querySelector(selector);
+        if (node) {
+            node.parentNode.removeChild(node);
+        }
+    }
     button.addEventListener("click", function () {
+        remove("a[href='#todo']");
+        remove("a[name='todo']");
         input.value = document.querySelector(".post-body").innerHTML.trim();
         input.select();
         document.execCommand("copy");
