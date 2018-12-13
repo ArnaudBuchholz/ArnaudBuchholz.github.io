db71cbf2c59c2b4b413a8571f5bebd9e15530dd0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-12-13 11:22:04 -0500

    Rely on meta for duration

--- decks/deck.js
+++ decks/deck.js
@@ -1,78 +1,102 @@
 "use strict";
 
-function processIncludes () {
-  return Promise.all([].slice.call(document.querySelectorAll("*[include]")).map(function (include) {
-    return gpf.http.get(include.getAttribute("include"))
-      .then(function (response) {
-        include.innerHTML = response.responseText;
-      })
-  }));
+function processIncludes() {
+    return Promise.all([].slice.call(document.querySelectorAll("*[include]")).map(function(include) {
+        return gpf.http.get(include.getAttribute("include"))
+            .then(function(response) {
+                include.innerHTML = response.responseText;
+            })
+    }));
 }
 
-function removeHiddenElements () {
-  [].slice.call(document.querySelectorAll(".hide")).forEach(function (node) {
-    node.parentNode.removeChild(node);
-  });
-}
-
-function buildTitle () {
-  document.title = document.querySelector("h3").innerHTML.replace(/<[^>]+>/g, "");
-}
-
-function buildAgenda () {
-  [].slice.call(document.querySelectorAll("h3"))
-    .filter(function (h3) {
-      return h3.getAttribute("data-agenda") !== "no";
-    })
-    .forEach(function (h3) {
-      var item = document.createElement("li");
-      [].slice.call(h3.childNodes, 0).forEach(function (node) {
-        item.appendChild(node.cloneNode(true));
-      });
-      this.appendChild(item);
-    }, document.getElementById("agenda"));
-}
-
-function reformatCodeSamples () {
-  // Replace tabs with double spaces in code samples to reduce code width
-  [].slice.call(document.querySelectorAll("code")).forEach(function (node) {
-    node.innerHTML = node.innerHTML.replace(/\t/g, "  ");
-  });
-}
-
-function processMeInfo () {
-  var
-    year = new Date().getFullYear(),
-    years = {
-      software: 1998,
-      js: 2008,
-      fe: 2013,
-      ui5: 2015
-    };
-  Object.keys(years).forEach(function (id) {
-    document.getElementById(id + "_years").innerHTML = year - years[id];
-  });
-}
-
-function loadReveal () {
-  gpf.require.define({Reveal: "reveal.js"}, function (require) {
-    "use strict";
-    require.Reveal.configure({
-      slideNumber: "c/t",
-      allottedTime: 40 * 60 * 1000, // 40 minutes
-      progressBarHeight: 10,
-      barColor: 'rgb(200,0,0)',
-      pausedBarColor: 'rgba(200,0,0,.6)'
+function removeHiddenElements() {
+    [].slice.call(document.querySelectorAll(".hide")).forEach(function(node) {
+        node.parentNode.removeChild(node);
     });
-  });
 }
 
-window.addEventListener("load", function () {
-  processIncludes()
-    .then(removeHiddenElements)
-    .then(buildTitle)
-    .then(buildAgenda)
-    .then(reformatCodeSamples)
-    .then(processMeInfo)
-    .then(loadReveal);
+function buildTitle() {
+    document.title = document.querySelector("h3").innerHTML.replace(/<[^>]+>/g, "");
+}
+
+function buildAgenda() {
+    [].slice.call(document.querySelectorAll("h3"))
+        .filter(function(h3) {
+            return h3.getAttribute("data-agenda") !== "no";
+        })
+        .forEach(function(h3) {
+            var item = document.createElement("li");
+            [].slice.call(h3.childNodes, 0).forEach(function(node) {
+                item.appendChild(node.cloneNode(true));
+            });
+            this.appendChild(item);
+        }, document.getElementById("agenda"));
+}
+
+function reformatCodeSamples() {
+    // Replace tabs with double spaces in code samples to reduce code width
+    [].slice.call(document.querySelectorAll("code")).forEach(function(node) {
+        node.innerHTML = node.innerHTML.replace(/\t/g, "  ");
+    });
+}
+
+function processMeInfo() {
+    var
+        year = new Date().getFullYear(),
+        years = {
+            software: 1998,
+            js: 2008,
+            fe: 2013,
+            ui5: 2015
+        };
+    Object.keys(years).forEach(function(id) {
+        document.getElementById(id + "_years").innerHTML = year - years[id];
+    });
+}
+
+function readDeckMeta(name, defaultValue) {
+    var meta = document.head.querySelector("meta[name='deck-" + name + "']"),
+        value;
+    if (meta) {
+        value = meta.getAttribute("content");
+        if (value) {
+            if (typeof defaultValue === "number") {
+                return parseInt(value, 10);
+            }
+            return value;
+        }
+    }
+    return defaultValue;
+}
+
+function loadReveal() {
+    var
+        deckLength = readDeckMeta("duration", 0),
+        deckProgressBarHeight = readDeckMeta("progress-bar-height", 10),
+        deckProgressBarColor = readDeckMeta("progress-bar-color", "rgb(200,0,0)"),
+        deckProgressBarPausedColor = readDeckMeta("progress-bar-paused-color", "rgba(200,0,0,.6)");
+    gpf.require.define({
+        Reveal: "reveal.js"
+    }, function(require) {
+        "use strict";
+        if (deckLength) {
+            require.Reveal.configure({
+                slideNumber: "c/t",
+                allottedTime: deckLength * 60 * 1000,
+                progressBarHeight: deckProgressBarHeight,
+                barColor: deckProgressBarColor,
+                pausedBarColor: deckProgressBarPausedColor
+            });
+        }
+    });
+}
+
+window.addEventListener("load", function() {
+    processIncludes()
+        .then(removeHiddenElements)
+        .then(buildTitle)
+        .then(buildAgenda)
+        .then(reformatCodeSamples)
+        .then(processMeInfo)
+        .then(loadReveal);
 });
