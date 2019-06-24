989cd207ecfc70240d7ae125e4006c2d0134806d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-06-24 10:57:12 -0400

    Do not block deck loading to detect the local environment

--- decks/training-ui5con18-opa.js
+++ decks/training-ui5con18-opa.js
@@ -1,22 +1,23 @@
 "use strict";
 
-document.write("<img id=\"running\" src=\"http://localhost:8080/running.png\" />");
-
 window.addEventListener("load", function() {
-    var isSampleAppAvailable = !!document.getElementById("running").naturalWidth;
-    // Links should open in a new tab to keep the presentation state
-    var missingSampleAppMsg = "Download the training-ui5con18-opa project and run\nnpm run-script serve";
-    window.missingSampleApp = function () {
-        alert(missingSampleAppMsg);
-    };
-    [].slice.call(document.querySelectorAll("a")).forEach(function (node) {
-        node.setAttribute("target", "_blank");
-        if (node.getAttribute("href").indexOf("http://localhost") === 0) {
-            if (!isSampleAppAvailable) {
-                node.setAttribute("title", missingSampleAppMsg);
-                node.setAttribute("href", "javascript:missingSampleApp()");
-                node.className = "missingSampleApp";
-            }
-        }
-    });
+    var img = document.createElement("img");
+    img.onerror = function () {
+      // Links should open in a new tab to keep the presentation state
+      var missingSampleAppMsg = "Download the training-ui5con18-opa project and run\nnpm run-script serve";
+      window.missingSampleApp = function () {
+          alert(missingSampleAppMsg);
+      };
+      [].slice.call(document.querySelectorAll("a"))
+          .filter(function (node) {
+              return node.getAttribute("href").indexOf("http://localhost") === 0;
+          })
+          .forEach(function (node) {
+              node.setAttribute("title", missingSampleAppMsg);
+              node.setAttribute("href", "javascript:missingSampleApp()");
+              node.className = "missingSampleApp";
+          });
+    }
+    img.setAttribute("src", "http://localhost:8080/running.png");
+    document.body.appendChild(img);
 });
