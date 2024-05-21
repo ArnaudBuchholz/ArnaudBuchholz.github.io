c0687362cc55a4bb1c23dfde5dcc9da07eb13e1f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2024-05-21 05:26:02 -0400

    chores(serve): update to REserve 2

--- index.json.dev.js
+++ index.json.dev.js
@@ -1,23 +1,19 @@
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
-const { promisify } = require('util')
-
-const readdirAsync = promisify(fs.readdir)
-const readFileAsync = promisify(fs.readFile)
-const statAsync = promisify(fs.stat)
+const { send } = require('reserve')
+const { readdir, readFile, stat } = require('node:fs/promises')
+const { join, extname, basename } = require('path')
 
 const twoWeeksAgo = new Date(new Date().getTime() - 2 * 7 * 24 * 60 * 60 * 1000)
 
 async function keepRecentFiles (folder, names) {
   for (let index = 0; index < names.length; ++index) {
-      const fullName = path.join(folder, names[index])
-      const stat = await statAsync(fullName)
-      if (stat.mtime < twoWeeksAgo) {
+      const fullName = join(folder, names[index])
+      const fileStat = await stat(fullName)
+      if (fileStat.mtime < twoWeeksAgo) {
           names[index] = undefined
       } else {
-          names[index] = { fullName, stat }
+          names[index] = { fullName, stat: fileStat }
       }
   }
   return names
@@ -29,32 +25,29 @@ async function keepRecentFiles (folder, names) {
 async function listRecentDecks (index) {
     const exposedDecks = index
       .filter(item => item.type === 'deck')
-      .map(item => path.basename(item.link))
-    const decks = (await readdirAsync('./decks'))
-      .filter(name => path.extname(name) === '.html')
+      .map(item => basename(item.link))
+    const decks = (await readdir('./decks'))
+      .filter(name => extname(name) === '.html')
       .filter(name => !(['me.html', 'disclaimer.html'].includes(name) || exposedDecks.includes(name)))
     return keepRecentFiles('./decks', decks)
 }
 
 async function listRecentBlogItems () {
-    const blogItems = (await readdirAsync('./blog/post'))
-      .filter(name => path.extname(name) === '.html')
+    const blogItems = (await readdir('./blog/post'))
+      .filter(name => extname(name) === '.html')
     return keepRecentFiles('./blog/post', blogItems)
 }
 
 module.exports = async (request, response) => {
-  const index = JSON.parse(await readFileAsync('./index.json'))
+  const index = JSON.parse(await readFile('./index.json'))
   const recentDecks = await listRecentDecks(index)
   const recentBlogItems = await listRecentBlogItems(index)
   recentBlogItems.concat(recentDecks).forEach(fullName => {
-      index.unshift({
-          type: 'wip',
-          link: fullName,
-          title: fullName
-      })
+    index.unshift({
+      type: 'wip',
+      link: fullName,
+      title: fullName
+    })
   })
-  response.writeHead(200, {
-    'Content-Type': 'application/json'
-  })
-  response.end(JSON.stringify(index))
+  return send(response, index)
 }
